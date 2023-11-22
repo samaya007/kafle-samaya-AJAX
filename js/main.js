c11ecfe3cc6a217d687a523414002da94c564830
Samaya Kafle <worksamaya@gmail.com>
2023-11-22 02:54:47 -0500

    Fixed image error
    Fixed image not showing error

--- js/main.js
+++ js/main.js
@@ -5,130 +5,111 @@
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
 
-  const spinner = `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_9Mto{animation:spinner_5GqJ 1.6s linear infinite;animation-delay:-1.6s}.spinner_bb12{animation-delay:-1s}@keyframes spinner_5GqJ{12.5%{x:13px;y:1px}25%{x:13px;y:1px}37.5%{x:13px;y:13px}50%{x:13px;y:13px}62.5%{x:1px;y:13px}75%{x:1px;y:13px}87.5%{x:1px;y:1px}}</style><rect class="spinner_9Mto" x="1" y="1" rx="1" width="10" height="10"/><rect class="spinner_9Mto spinner_bb12" x="1" y="1" rx="1" width="10" height="10"/></svg>`;
 
-
-
-
-
-
+  const spinner =
+      `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_9Mto{animation:spinner_5GqJ 1.6s linear infinite;animation-delay:-1.6s}.spinner_bb12{animation-delay:-1s}@keyframes spinner_5GqJ{12.5%{x:13px;y:1px}25%{x:13px;y:1px}37.5%{x:13px;y:13px}50%{x:13px;y:13px}62.5%{x:1px;y:13px}75%{x:1px;y:13px}87.5%{x:1px;y:1px}}</style><rect class="spinner_9Mto" x="1" y="1" rx="1" width="10" height="10"/><rect class="spinner_9Mto spinner_bb12" x="1" y="1" rx="1" width="10" height="10"/></svg>`;
 
   // Functions
   function modelLoaded() {
-    hotspots.forEach((hotspot) => {
-      hotspot.style.display = "block";
-    });
+      hotspots.forEach((hotspot) => {
+          hotspot.style.display = "block";
+      });
   }
-
-
 
   function loadInfoBoxes() {
-    // Make AJAX call here using Fetch API
-    fetch("https://swiftpixel.com/earbud/api/infoboxes")
-      .then(response => response.json())
-      .then(data => {
-        data.forEach((infoBox, index) => {
-          let selected = document.querySelector(`#hotspot-${index + 1}`);
-  
-          const titleElement = document.createElement("h2");
-          titleElement.textContent = infoBox.heading;
-  
-          const textElement = document.createElement("p");
-          textElement.textContent = infoBox.description;
-  
-          const thumbnailElement = document.createElement("img");
-          thumbnailElement.src = `images/${infoBox.thumbnail}`;
-          thumbnailElement.alt = infoBox.heading;
-  
-          selected.appendChild(titleElement);
-          selected.appendChild(textElement);
-          selected.appendChild(thumbnailElement);
-        });
-      })
-      .catch(error => console.error("Error fetching info boxes:", error));
+      fetch("https://swiftpixel.com/earbud/api/infoboxes")
+          .then(response => response.json())
+          .then(data => {
+              data.forEach((infoBox, index) => {
+                  let selected = document.querySelector(
+                      `#hotspot-${index + 1}`);
+
+                  const thumbnailElement = document.createElement("img");
+                  thumbnailElement.src = `images/${infoBox.thumbnail}`;
+
+
+
+                  const titleElement = document.createElement("h2");
+                  titleElement.textContent = infoBox.heading;
+
+                  const textElement = document.createElement("p");
+                  textElement.textContent = infoBox.description;
+
+
+
+                  selected.appendChild(titleElement);
+                  selected.appendChild(textElement);
+                  selected.appendChild(thumbnailElement);
+              });
+          })
+          .catch(error => console.error("Error getting info boxes:", error));
   }
-  
-
-
-
-
-
-
 
   function loadMaterialInfo() {
-    // Make AJAX call here using Fetch API
 
-materialList.innerHTML=spinner;
+      //this one is for spinner animation, if i plan to add it on my portfolio make sure to change the materialList class
+      materialList.innerHTML = spinner;
 
+      fetch("https://swiftpixel.com/earbud/api/materials")
 
+          .then(response => response.json())
 
-    fetch("https://swiftpixel.com/earbud/api/materials")
+          .then(data => {
+              materialList.innerHTML = "";
 
+              data.forEach((material) => {
 
-    
-      .then(response => response.json())
+                  const clone = materialTemplate.content.cloneNode(true);
 
+                  const materialHeading = clone.querySelector(
+                      ".material-heading");
+                  materialHeading.textContent = material.heading;
 
-      
-      .then(data => {
-        materialList.innerHTML="";
+                  const materialDescription = clone.querySelector(
+                      ".material-description");
+                  materialDescription.textContent = material.description;
 
-        data.forEach((material) => {
-          // Make a copy of the template
-          const clone = materialTemplate.content.cloneNode(true);
+                  materialList.appendChild(clone);
 
-          const materialHeading = clone.querySelector(".material-heading");
-          materialHeading.textContent = material.heading;
+              });
 
-          const materialDescription = clone.querySelector(".material-description");
-          materialDescription.textContent = material.description;
+          })
 
-          // Append the populated template to the ul
-          materialList.appendChild(clone);
-
-
-          
-        });
-
-
-
-      })
-
-
-
-      .catch(error => console.error("Error fetching materials:", error));
-
-
-
-
+          .catch(error => console.error("Error getting materials:", error));
 
   }
 
-
-
+  // loads data from api
+  loadInfoBoxes();
+  loadMaterialInfo();
 
 
 
 
   function showInfo() {
-    let selected = document.querySelector(`#${this.slot}`);
-    gsap.to(selected, 1, { autoAlpha: 1 });
+      let selected = document.querySelector(`#${this.slot}`);
+      gsap.to(selected, 1, {
+          autoAlpha: 1
+      });
   }
 
   function hideInfo() {
-    let selected = document.querySelector(`#${this.slot}`);
-    gsap.to(selected, 1, { autoAlpha: 0 });
+      let selected = document.querySelector(`#${this.slot}`);
+      gsap.to(selected, 1, {
+          autoAlpha: 0
+      });
   }
+
+
+
 
   // Event listeners
   model.addEventListener("load", modelLoaded);
 
-  hotspots.forEach(function (hotspot) {
-    hotspot.addEventListener("mouseenter", showInfo);
-    hotspot.addEventListener("mouseleave", hideInfo);
+  hotspots.forEach(function(hotspot) {
+      hotspot.addEventListener("mouseenter", showInfo);
+      hotspot.addEventListener("mouseleave", hideInfo);
   });
 
-  // Load data from APIs
-  loadInfoBoxes();
-  loadMaterialInfo();
+
 })();
